04c2fd2348b982981a7c2bb93074f8697629e9b5
SagarParmarr <sagar.parmar7006@gmail.com>
2025-05-20 15:27:59 +0530

    feat: railway.tomal file and delete other unused files

--- src/main.py
+++ src/main.py
@@ -86,11 +86,6 @@ app.add_middleware(
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.environ.get("PORT", 8000))
-
     uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=port,
-        reload=True,
+        "main:app", host="0.0.0.0", port=8000, reload=False  # <- remove or set to False
     )
